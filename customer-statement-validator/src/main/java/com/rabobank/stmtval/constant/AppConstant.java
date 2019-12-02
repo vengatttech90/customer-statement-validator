44df5dd6664051c9a71a9b3a47f3c4a18e6bbcef
vengatraman <vengattech90@gmail.com>
2019-12-02 11:02:44 +0530

    update.

--- customer-statement-validator/src/main/java/com/rabobank/stmtval/constant/AppConstant.java
+++ customer-statement-validator/src/main/java/com/rabobank/stmtval/constant/AppConstant.java
@@ -2,6 +2,6 @@ package com.rabobank.stmtval.constant;
 
 public class AppConstant {
 	public static final String columns[] = { "Reference Number", "Description" };
-	public static final String reportName = "Customer_Statement_Validated_Report";
+	public static final String reportName = "Customer_Statement_Validated_Report.xlsx";
 	public static final String sheetName = "Validated_Statement";
 }
